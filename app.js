abe6c56e64e3a59f8d62b138086520afff6cac8b
shubhradip <shubhradipsarkar@gmail.com>
2024-03-09 09:43:25 +0530

    websocket removed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const { RootSchema } = require('./schema/schema');
 const mongoose = require('mongoose');
@@ -6,8 +7,28 @@ require("dotenv").config();
 const app = express()
 const cors = require('cors');
 const auth = require("./routes/auth");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||4000;
 const verifyToken = require("./middleware/authMiddleware")
+// const { Server } = require("socket.io")
+// var http = require('http');
+// var server = http.createServer(app);
+// var io = new Server(server,{
+//     cors:{
+//         origin: '*'
+//     }
+// });
+// //const server = new Server(app)
+// io.on("connection", (socket)=>{
+//     console.log("User connected");
+//     console.log("ID", socket.id);
+//     socket.emit("welcome", `welcome to the server ${socket.id}`);
+//     socket.broadcast.emit("welcome", `${socket.id} joined the server...`);
+
+//     socket.on("message", ({message, room})=>{
+//         //console.log(data);
+//         io.to(room).emit("receive",message);   
+//     })
+// })
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{console.log("Mongodb Connected")})
@@ -23,6 +44,7 @@ app.use('/api' ,verifyToken, graphqlHTTP({   // For other queries and updates
     schema: RootSchema,
     graphiql: true,  
 }));
+
 
 app.listen(PORT, ()=>console.log(`Server is running at PORT ${PORT}`));
 
