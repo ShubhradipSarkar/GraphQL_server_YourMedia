66ecdd0bda26a62e3bf70b268f23602638aceb33
shubhradip <shubhradipsarkar@gmail.com>
2024-04-09 11:23:14 +0530

    select pointer

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ require("dotenv").config();
 const app = express()
 const cors = require('cors');
 const auth = require("./routes/auth");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||4000;
 const verifyToken = require("./middleware/authMiddleware")
 
-mongoose.connect(process.env.MONGO_URL||4000)
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>{console.log("Mongodb Connected")})
 .catch((err)=>{console.log("Couldn't connect to Database", err)});
 
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({encoded: false}))
 
 app.use('/auth', auth);   // For register and log in
-app.use('/api' ,verifyToken, graphqlHTTP({   // For other queries and updates
+app.use('/api' , graphqlHTTP({   // For other queries and updates
     schema: RootSchema,
     graphiql: true,  
 }));
